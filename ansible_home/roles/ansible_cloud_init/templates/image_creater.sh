982573252a20c7660fa9c5e07e592f34735d3769
DanielMueller1309 <danielmueller-dm@gmx.de>
2021-05-17 14:06:06 +0200

    dont know what i do

--- ansible_home/roles/ansible_cloud_init/templates/image_creater.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-qemu-img create -f qcow2 -o backing_file=/var/lib/libvirt/images/base/ubuntu-20.04.qcow2 /var/lib/libvirt/images/instance-1/instance-1.qcow2
-qemu-img resize /var/lib/libvirt/images/instance-1/instance-1.qcow2 5G
